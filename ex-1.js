82f5c558b4916a6e76fc1cf0a5017d266230a618
Kittanut Thammapibaljit <tkittanut@gmail.com>
2024-07-17 14:27:01 +0700

    finish assignment

--- ex-1.js
+++ ex-1.js
@@ -1,5 +1,41 @@
 function findStudentIndex(students, searchStudent) {
-	// เริ่มเขียนโค้ดตรงนี้จ้า
+  let left = 0;
+  let right = students.length - 1;
+
+  let index = -1;
+  // เริ่มเขียนโค้ดตรงนี้จ้า
+  while (left <= right) {
+    let middle = Math.floor((left + right) / 2);
+
+    if (students[middle] === searchStudent) {
+      index = middle;
+      return index;
+    } else if (students[middle] < searchStudent) {
+      left = middle + 1;
+    } else {
+      right = middle - 1;
+    }
+  }
+  return index;
 }
 
 // ตอบคำถามตรงนี้จ้า
+/*	binary search
+	big O = O(log N)
+	เพราะ มันตัดงานออกทีละครึ่งนึง เลยไว 
+*/
+
+const students = [
+  "Andrew",
+  "Bobby",
+  "Diana",
+  "George",
+  "Hannah",
+  "Isaac",
+  "Jasmine",
+  "John",
+];
+
+const searchStudent = "John";
+console.log(findStudentIndex(students, searchStudent));
+console.log(findStudentIndex(students, "DF Park love dog, not cat."));
--- ex-2.js
+++ ex-2.js
@@ -1,3 +1,34 @@
 function findBookIndex(books, searchTitle) {
   // เริ่มเขียนโค้ดตรงนี้จ้า
+  let left = 0;
+  let right = books.length - 1;
+  let index = -1;
+
+  while (left <= right) {
+    let middle = Math.floor((left + right) / 2);
+    if (books[middle].title === searchTitle) {
+      index = middle;
+      return index;
+    } else if (books[middle].title < searchTitle) {
+      left = middle + 1;
+    } else {
+      right = middle - 1;
+    }
+  }
+  return index;
 }
+
+const books = [
+  { title: "A Tale of Two Cities", author: "Charles Dickens" },
+  { title: "Brave New World", author: "Aldous Huxley" },
+  { title: "Catch-22", author: "Joseph Heller" },
+  { title: "Don Quixote", author: "Miguel de Cervantes" },
+  { title: "Ender's Game", author: "Orson Scott Card" },
+  { title: "Fahrenheit 451", author: "Ray Bradbury" },
+  { title: "Gone with the Wind", author: "Margaret Mitchell" },
+  { title: "Harry Potter", author: "J.K. Rowling" },
+];
+
+const searchBook = "Gone with the Wind";
+console.log(findBookIndex(books, searchBook));
+console.log(findBookIndex(books, "The Master Mind"));
